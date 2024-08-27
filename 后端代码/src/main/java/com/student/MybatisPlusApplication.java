a8c845b94794a382a2eb80839917466aaaaaf766
liuhaowest <2731363939@qq.com>
2024-08-27 21:04:31 +0800

    first commit

---
+++ 后端代码/src/main/java/com/student/MybatisPlusApplication.java
@@ -0,0 +1,21 @@
+package com.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 前端vue： npm run dev
+ *
+ *   1. 管理员：111909010418   123456
+ *   2. 教师:   63513   314833
+ *   3. 学生:   311909020308    267723
+ * springboot_student
+ */
+@SpringBootApplication
+public class MybatisPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusApplication.class, args);
+    }
+
+}
